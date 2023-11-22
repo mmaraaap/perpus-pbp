a8093a837b76684a758e6529c1b8f6daf642be7e
mmaraaap <ammara.pranahiza@ui.ac.id>
2023-11-22 09:21:27 +0700

    tugas 9

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponseNotFound, HttpResponseRedirect, JsonResponse
 from main.forms import ProductForm
@@ -166,3 +167,21 @@ def add_product_ajax(request):
 
     return HttpResponseNotFound()
 
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            amount = int(data["amount"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
